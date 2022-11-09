15792f427601890306778e12b29a2ef9f9703d8d
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-09 15:29:50 +0100

    ok

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
-// numeri da 1 a 100
-const div = document.querySelector("div.container");
-for (var i = 1; i < 101; i++) {
-    const element= `<div class="box">${i}</div>`;
-    div.innerHTML+= element;
-    // se i diviso 15 da come resto 0 è divisibile per 3 e 5
-    if ((i % 3 == 0) && (i % 5 == 0)) 
-    console.log(`${element} fizzbuzz`);
-    // se i diviso 3 da come resto 0 è divisibile per 3
-    else if (i % 3 == 0)
-     console.log("divisibile3");
-    // se i diviso 5 da come resto 0 è divisibile per 5
-    else if (i % 5 == 0) 
-    console.log("dibisibile5");
-//    altrimenti stampa dirattamente il valore
-    else
-    console.log(i);
+let container = document.getElementById("container"); 
+// Definisco la variabile container associandola al corrispettivo elemento HTML
+
+
+for (let i = 1; i <= 100; i++) { 
+// Genera numeri da 1 a 100
+
+  if((i%3 == 0) && (i%5 == 0)){ 
+
+    container.innerHTML += '<div class="box fizzbuzz">FizzBuzz</div>';
+    // Stampa FizzBuzz se multiplo di 3 e 5
+
+  } else if((i%3 == 0)) { 
+
+    container.innerHTML += '<div class="box fizz">Fizz</div>';
+    // Stampa Fizz se multipplo di 3
+
+  } else if ((i%5 == 0)) { 
+
+    container.innerHTML += '<div class="box buzz">Buzz</div>';
+    // Stampa Buzz se multipplo di 5
+  } else {
+    
+    container.innerHTML += '<div class="box">' + i + '</div>';
+    // stampa i numeri
 }
- 
+
+}
