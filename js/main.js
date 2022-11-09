d3866e0b731eca1de10ea445d47285d2f30f5f9a
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-09 13:22:38 +0100

    altra soluzione forse va un branch ma ok XD

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 for (var i = 1; i < 101; i++) {
     // se i diviso 15 da come resto 0 è divisibile per 3 e 5
-    if (i % 15 == 0) console.log("divisiible per 3 e 5");
+    if ((i % 3 == 0) && (i % 5 == 0)) console.log("divisiible per 3 e 5");
     // se i diviso 3 da come resto 0 è divisibile per 3
     else if (i % 3 == 0) console.log("divisibile3");
     // se i diviso 5 da come resto 0 è divisibile per 5
